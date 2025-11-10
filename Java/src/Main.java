69e4eeb0b21365103be5db59e443e0178bf888f8
Stas Koshynskyi <stasony100@gmail.com>
2025-11-10 11:29:42 +0200

    Додав клас який виводить ааа

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,11 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("!");
-
-        for (int i = 1; i <= 5; i++) System.out.println("1asddasasd");
+import acm.program.ConsoleProgram;
+public class Main extends ConsoleProgram {
+    public void run(){
+        print("aa");
+    }
+    public static void main(String[] args){
+        new Main().start(args);
     }
 }
