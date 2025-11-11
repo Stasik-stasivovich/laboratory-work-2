bf2aaa80c7815da0c31c8e08a50bb071b876d4c4
Stas Koshynskyi <stasony100@gmail.com>
2025-11-11 12:00:03 +0200

    Поправив структуру і додав тобі коментарі

--- Java/src/Breakout.java
+++ Java/src/Breakout.java
@@ -10,26 +10,28 @@ public class Breakout extends GraphicsProgram {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 600;
     private static final int BRICK_HEALTH = 1;
-    private int BALL_RADIUS = 10;
+    private static final int BALL_RADIUS = 10;
     private int vx = 0;
     private int vy = 0;
 
-    private static int DELAY = 50;
+    private static final int DELAY = 50;
 
     private static final int MAX_VX = 15;
     private static final int MAX_VY = 10;
-    private static final int  PADDLE_WIDTH = 60;
+    private static final int PADDLE_WIDTH = 60;
     private static final int PADDLE_HEIGHT = 10;
     private static final int PADDLE_Y_OFFSET = 20;
 
+    private boolean isChoisedLevel = false;
+    private int level = -1;
+    private boolean isChoisedResult = false;
 
-    //private GCompound choiseMenu = createChoiseMenu; // оце маєш написати
+    private static final int PLAYER_STARTHP = 2;
 
-
-    private int playerHealth = 2;
+    private int playerHealth = PLAYER_STARTHP;
     private boolean gameOver;
 
-    private GOval ball = new GOval(getWidth()/2-BALL_RADIUS,getHeight() - PADDLE_Y_OFFSET - BALL_RADIUS - PADDLE_HEIGHT - 20,BALL_RADIUS*2,BALL_RADIUS*2);
+    private GOval ball ;
     private RandomGenerator random = RandomGenerator.getInstance();
 
     private GObject collision;
@@ -37,20 +39,58 @@ public class Breakout extends GraphicsProgram {
     public void run(){
         setSize(WIDTH,HEIGHT);
         addMouseListeners();
+        while (true) {
+            startProgram();
+        }
+    }
+
+    private void startProgram() {
+/*
+        showChoiseMenu(); // оце ти пишеш
+        waitForChoiseLevel();
+*/
         setup();
 
         startGame();
+/*
+        showResultMenu(); // це ти пишеш
+        waitForChoiseResult();
+*/
+    }
+
+    private void showResultMenu() {
+        removeAll();
+        isChoisedResult = true;
+        // отут пишеш
+    }
+
+    private void showChoiseMenu() {
+        removeAll();
+        isChoisedLevel = true;
+        // тут пишеш
+    }
+
+    private void waitForChoiseResult() {
+        while(isChoisedResult)pause(100);
+    }
+
+    private void waitForChoiseLevel() {
+        while (isChoisedLevel)pause(100);
     }
 
     private void setup() {
-
+        removeAll();
         gameOver = false;
+        playerHealth = PLAYER_STARTHP;
 
-
-        ball.setLocation(getWidth()/2-BALL_RADIUS,getHeight() - PADDLE_Y_OFFSET - BALL_RADIUS - PADDLE_HEIGHT - 20);
-
-
+        ball = new GOval(getWidth()/2-BALL_RADIUS,
+                getHeight() - PADDLE_Y_OFFSET - BALL_RADIUS - PADDLE_HEIGHT - 20
+                ,BALL_RADIUS*2,BALL_RADIUS*2);
         add(ball);
+
+        /*
+        Отут рівень додаєш який вибрали
+         */
 
 
 
@@ -69,7 +109,7 @@ public class Breakout extends GraphicsProgram {
                     gameOver = true;
                 }
 
-            }
+            }// це я потім зроблю
             /*collision = checkForCollision();
             if(collision != null){
                 changeDirectory();
@@ -79,7 +119,7 @@ public class Breakout extends GraphicsProgram {
     }
 
     private boolean checkIfOutOfBound() {
-        return ball.getY()+BALL_RADIUS*2>getWidth();
+        return ball.getY()+BALL_RADIUS*2>getHeight();
     }
 
     private void moveBall() {
@@ -90,13 +130,18 @@ public class Breakout extends GraphicsProgram {
 
 
     public void mouseClicked(MouseEvent e) {
-        //if (!choiseMenu.isVisible()/* && !endMenu.isVisible*/){
-            if (!gameOver && vx==0){
+        if (isChoisedResult){
+            // отут логіка кнопок
+            // потім не забуть поміняти
+            // isChoisedResult = false;
+        }
+        if (isChoisedLevel){}
 
-                vy = - random.nextInt(5,MAX_VY);
-                vx = random.nextInt(5,MAX_VX);
-                if (random.nextBoolean(0.5)) vx = - vx;
-            //}
+        if (!gameOver && vx==0){
+
+            vy = - random.nextInt(5,MAX_VY);
+            vx = random.nextInt(5,MAX_VX);
+            if (random.nextBoolean(0.5)) vx = - vx;
         }
     }
 
