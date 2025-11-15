18ebf534424a7cd6c1299bd949eb3fdd257a8edd
Programing-Java <bogdankravchuk.game@gmail.com>
2025-11-16 00:29:23 +0200

    Додав GCompound. Рівень 1 -  стіна з блоків працює відмінно. Рівень 2 - піраміда працює відмінно. В класі Brick extends GCompound є створення блоків  GRect та створення здоров'я  GLabel

--- Java/src/Brick.java
+++ Java/src/Brick.java
@@ -1,11 +1,13 @@
+import acm.graphics.GCompound;
+import acm.graphics.GLabel;
 import acm.graphics.GRect;
 
 import java.awt.*;
 //клас цеглинок
-public class Brick extends GRect {
-    private double width_of_brick;
-    private double height_of_brick;
+public class Brick extends GCompound {
     private Color color_of_brick;
+    //private GLabel health_of_brick;
+    private  GRect brick;
 
 
     /**
@@ -13,13 +15,34 @@ public class Brick extends GRect {
      *
      * @param width_of_brick_parameter  - довжина цеглинок
      * @param height_of_brick_parameter - висота цеглинок
+     * @param health_of_brick_parameter - здоров'я цеглинок
+     *
+     * Grect brick - цеглинка
+     * Glabel health - здоров'я цеглинок
+     *
+     *  координати здоров'я цеглинок:
+     *  Х = (width_of_brick_parameter-health.getWidth())/2,
+     *  У = (height_of_brick_parameter-health.getAscent()/2)
+     *
+     *  шрифт (int) (height_of_brick_parameter/2)
+     *
      */
-    public Brick(double width_of_brick_parameter, double height_of_brick_parameter) {
-        super( width_of_brick_parameter, height_of_brick_parameter);
-        this.width_of_brick = width_of_brick_parameter;
-        this.height_of_brick = height_of_brick_parameter;
-        this.setColor(color_of_brick);
-        this.setFilled(true);
+    public Brick(double width_of_brick_parameter, double height_of_brick_parameter, int health_of_brick_parameter) {
+         final Font font_of_brick = new Font("PT Serif", Font.BOLD, (int) (height_of_brick_parameter/2));
+
+       brick = new GRect(width_of_brick_parameter, height_of_brick_parameter);
+        add(brick);
+
+
+        GLabel health = new GLabel(Integer.toString(health_of_brick_parameter));
+        health.setFont(font_of_brick);
+        health.setLocation((width_of_brick_parameter-health.getWidth())/2, (height_of_brick_parameter-health.getAscent()/2));
+        health.setColor(Color.BLACK);
+        add(health);
+
+
+
+
     }
 
     /**
@@ -27,9 +50,14 @@ public class Brick extends GRect {
      *
      * @param color_of_brick_parameter - setColor of brick - встановлення кольору цеглинок через сеттер
      */
-    public void setColor_of_brick(Color color_of_brick_parameter) {
+   public void setColor_of_brick(Color color_of_brick_parameter) {
         this.color_of_brick = color_of_brick_parameter;
-        this.setFillColor(color_of_brick_parameter);
+        brick.setColor(color_of_brick);
+        brick.setFilled(true);
     }
+
+
+
+
 
 }
--- Java/src/Create_Level.java
+++ Java/src/Create_Level.java
@@ -68,7 +68,7 @@ public class Create_Level {
         double step_y = 0.005 * getHeight;
         int amount_of_brick_y = 6;
         int amount_of_brick_x = 10;
-        double width_of_brick = ((getWidth-(10*step_x)) / amount_of_brick_x);
+        double width_of_brick = ((getWidth-(amount_of_brick_x*step_x)) / amount_of_brick_x);
         double height_of_brick = (getHeight/3) / amount_of_brick_y;
         double x_of_brick = 0;
         double y_of_brick = 0;
@@ -80,24 +80,21 @@ public class Create_Level {
             //цикл будування ширини 10 стовпців
             for (int x = 0; x < amount_of_brick_x; x++) {
                 //створення обєкту - цеглинки
-                Brick brick1 = new Brick(width_of_brick, height_of_brick);
+                Brick brick1 = new Brick( width_of_brick, height_of_brick, health_parameter);
 
 
-//створення обєкту Glabel health - здоров'я цеглинки
-                GLabel health_of_breaks = new GLabel(Integer.toString(health_parameter));
 
 
-                health_of_breaks.setFont(new Font("PT Serif", Font.BOLD, (int) height_of_brick/2));
-                health_of_breaks.setLocation(((width_of_brick-health_of_breaks.getWidth())/2)+x_health, (height_of_brick-health_of_breaks.getHeight()/2) +y_health);
-                //умова кольору
+               //умова кольору
                 if (y % 2 == 0)
                     brick1.setColor_of_brick(Color.BLUE);
 
                 else
                     brick1.setColor_of_brick(Color.YELLOW);
+
+
                 level.add(brick1);
-                level.add(health_of_breaks);
-                brick1.setLocation(x_of_brick, y_of_brick);
+                brick1.setLocation(x_of_brick, 1+y_of_brick);
 
                 x_of_brick += (step_x + width_of_brick);
                 x_health += (step_x + width_of_brick);
@@ -133,8 +130,6 @@ public class Create_Level {
      *         double width_of_brick = ((getWidth-(brick_in_piramide*step_x)) / brick_in_piramide); - ширина цеглинки
      *         double height_of_brick = (getHeight/3) / brick_in_piramide; - висота цеглинки
      *         double x_of_brick = 0; - потім дорівнює x_of_brick += (step_x + width_of_brick/2); - це зсув блоку по ширині (щоб піраміда мала нормальний вигляд)
-     *         double x_health= 0;
-     *         double y_health= 0;
      *
      *
      *         Початкові координати прямокутника піраміди:
@@ -144,73 +139,46 @@ public class Create_Level {
     private static void build_level_2(GCompound level, double getWidth, double getHeight, int health_parameter) {
         double step_x = 0.005 * getWidth;
         double step_y = 0.005 * getHeight;
-        int brick_in_piramide = 3;
-        int y_brick_in_piramide=brick_in_piramide;
-        double width_of_brick = ((getWidth-(brick_in_piramide*step_x)) / brick_in_piramide);
-        double height_of_brick = (getHeight/3) / brick_in_piramide;
+        int brick_in_piramide = 10;
+        int y_brick_in_piramide = brick_in_piramide;
+        double width_of_brick = ((getWidth - (brick_in_piramide * step_x)) / brick_in_piramide);
+        double height_of_brick = (getHeight / 3) / brick_in_piramide;
         double x_of_brick = 0;
-        double x_health= 0;
-        double y_health= 0;
 
 
 
-
-
-
-
-
-
-        for (int y = 0; y <y_brick_in_piramide; y++) {
+        for (int y = 0; y < y_brick_in_piramide; y++) {
             for (int x = 0; x < brick_in_piramide; x++) {
 
+               Brick brick2 = new Brick(width_of_brick, height_of_brick, health_parameter);
 
-
-                Brick brick2 = new Brick(width_of_brick, height_of_brick);
-
-                //створення обєкту Glabel health - здоров'я цеглинки
-                GLabel health_of_breaks = new GLabel(Integer.toString(health_parameter));
-
-                health_of_breaks.setFont(new Font("PT Serif", Font.BOLD, (int) height_of_brick/2));
-                //health_of_breaks.setLocation(((width_of_brick-health_of_breaks.getWidth())/2)+x_health,brick_in_piramide*(height_of_brick+step_y) - (health_of_breaks.getHeight()/2)-y_health );
-                //умова кольору
                 if (y % 2 == 0)
                     brick2.setColor_of_brick(Color.BLUE);
 
                 else
                     brick2.setColor_of_brick(Color.YELLOW);
                 level.add(brick2);
-                level.add(health_of_breaks);
 
                 brick2.setLocation(x_of_brick+ x*(width_of_brick+step_x), ((y_brick_in_piramide-1)*(height_of_brick)+(step_y*(y_brick_in_piramide-1)))+1- y*(height_of_brick+step_y));
-                brick2.setColor_of_brick(Color.WHITE);
-                brick2.setColor(Color.BLACK);
-             //   x_health+= (step_x + width_of_brick);
-            }
-           // x_health=width_of_brick/2;
-            //y_health+= (step_y + height_of_brick);
-            brick_in_piramide--;
-            x_of_brick += (step_x + width_of_brick/2);
-        }
 
-        /*
-       ДОРОБИТИ КООРДИНАТИ РОЗМІЩЕННЯ ЗДОРОВ'Я ЦЕГЛИНОК
-         */
+            }
+            brick_in_piramide--;
+            x_of_brick += (step_x + width_of_brick)/2;
+
+
+            }
+
 
 // створення Glabel - назва рівня "Рівень 2"
-        GLabel level_name_1 = new GLabel("Рівень 2", 0, getHeight);
-        level_name_1.setFont(new Font("PT Serif", Font.BOLD, (int) height_of_brick/2));
-        level.add(level_name_1);
+            GLabel level_name_1 = new GLabel("Рівень 2", 0, getHeight);
+            level_name_1.setFont(new Font("PT Serif", Font.BOLD, (int) height_of_brick / 2));
+            level.add(level_name_1);
+
+
+        }
 
 
     }
-
-
-
-
-
-
-
-}
 
 
 
