03b6d155c3620faf6cea48f480be1f23874a9865
Stas Koshynskyi <stasony100@gmail.com>
2025-11-17 13:18:10 +0200

    Додав клас бонусів

--- Java/src/BonusLinkedList.java
+++ Java/src/BonusLinkedList.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class BonusLinkedList {
+    public BonusNode head;
+    public BonusNode tail;
+    public BonusLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+    public void add(Bonus bonus) {
+        if (this.head == null) {
+            this.head = new BonusNode(bonus);
+            this.tail = this.head;
+        }
+        else {
+            tail.next = new BonusNode(bonus);
+            tail = tail.next;
+        }
+    }
+    public BonusNode get(int index) {
+        BonusNode temp = this.head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
